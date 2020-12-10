2b6de5bd643deedd40a453fc80731d0fb1914435
unknown <xxspanzie@gmail.com>
2020-12-10 23:07:16 +0700

    50% remaining

---
+++ script.js
@@ -0,0 +1,39 @@
+// const overlay= document.querySelector('img.overlay');
+// overlay.addEventListener('mouseover', function(){
+//     overlay.classList.add('bg');
+// });
+
+// overlay.addEventListener('mouseout', function(){
+//     overlay.classList.remove('bg');
+// });
+
+const fa= document.querySelectorAll('i.active')[0];
+fa.addEventListener('click', function(){
+    fa.classList.remove('far');
+    fa.classList.add('fas');
+});
+const fa1= document.querySelectorAll('i.active')[1];
+fa1.addEventListener('click', function(){
+    fa1.classList.remove('far');
+    fa1.classList.add('fas');
+});
+const fa2= document.querySelectorAll('i.active')[2];
+fa2.addEventListener('click', function(){
+    fa2.classList.remove('far');
+    fa2.classList.add('fas');
+});
+const fa3= document.querySelectorAll('i.active')[3];
+fa3.addEventListener('click', function(){
+    fa3.classList.remove('far');
+    fa3.classList.add('fas');
+});
+const fa4= document.querySelectorAll('i.active')[4];
+fa4.addEventListener('click', function(){
+    fa4.classList.remove('far');
+    fa4.classList.add('fas');
+});
+const fa5= document.querySelectorAll('i.active')[5];
+fa5.addEventListener('click', function(){
+    fa5.classList.remove('far');
+    fa5.classList.add('fas');
+});
